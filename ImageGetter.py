8572c4df525910f6065dfd14267ee8b388227821
MikkelBlaesbjerg <94799892+MikkelBlaesbjerg@users.noreply.github.com>
2022-11-04 21:28:47 +0100

    Parallel processes to speed up data collection

--- ImageGetter.py
+++ ImageGetter.py
@@ -4,61 +4,93 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import requests
 from PIL import Image
+def image_getter(area: str):
+    num_images = 1000
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    countries = [area]
+    # Get html from randomstreetview.com
+    for country in countries:
+        driver.get(f"https://www.randomstreetview.com/{country}")
+        # Load images
+        im = 0
+        while im < num_images:
+            # Get html
+            html = driver.page_source
+            # Find the div address and store it in a variable
+            address = driver.find_element(By.XPATH, '//*[@id="address"]').get_attribute('textContent')
+            # Find where "google.com/maps" is in the html
+            start = html.find("google.com/maps")
+            # Find where that string ends
+            end = html.find('"', start)
+            # Get the url
+            url = html[start:end]
+            # Remove anything before data
+            url = url[url.find("data"):]
+            # Remove before !1s
+            url = url[url.find("!1s")+3:]
+            # Remove after !
+            panoid = url[:url.find("!")]
+            if panoid == "":
+                driver.refresh()
+                print("Missing link")
+                continue
+            images = []
+            for x in [0, 1, 2, 3]:
+                for y in [0, 1]:
+                    url = f"https://streetviewpixels-pa.googleapis.com/v1/tile?cb_client=apiv3&panoid={panoid}&output=tile&x={x}&y={y}&zoom=2&nbt=1&fover=2"
+                    # Get the image from the url wth Image
+                    image = Image.open(requests.get(url, stream=True).raw)
+                    # Add the image to the list
+                    images.append(image)
+
+            # Stich the images together
+            # Create a new image
+            new_image = Image.new("RGB", (2048, 1024))
+            # Paste the images
+            new_image.paste(images[0], (0, 0))
+            new_image.paste(images[1], (0, 512))
+            new_image.paste(images[2], (512, 0))
+            new_image.paste(images[3], (512, 512))
+            new_image.paste(images[4], (1024, 0))
+            new_image.paste(images[5], (1024, 512))
+            new_image.paste(images[6], (1536, 0))
+            new_image.paste(images[7], (1536, 512))
 
 
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-countries = ['dk']
-# Get html from randomstreetview.com
-for country in countries:
-    driver.get(f"https://www.randomstreetview.com/{country}")
-    # Load images
-    for im in range (10):
-        # Get html
-        html = driver.page_source
-        # Find the div address and store it in a variable
-        address = driver.find_element(By.XPATH, '//*[@id="address"]').get_attribute('textContent')
-        # Find where "google.com/maps" is in the html
-        start = html.find("google.com/maps")
-        # Find where that string ends
-        end = html.find('"', start)
-        # Get the url
-        url = html[start:end]
-        # Remove anything before data
-        url = url[url.find("data"):]
-        # Remove before !1s
-        url = url[url.find("!1s")+3:]
-        # Remove after !
-        panoid = url[:url.find("!")]
-        if panoid == "":
-            im -= 1
-            continue
-        images = []
-        for x in [0, 1, 2, 3]:
-            for y in [0, 1]:
-                url = f"https://streetviewpixels-pa.googleapis.com/v1/tile?cb_client=apiv3&panoid={panoid}&output=tile&x={x}&y={y}&zoom=2&nbt=1&fover=2"
-                # Get the image from the url wth Image
-                image = Image.open(requests.get(url, stream=True).raw)
-                # Add the image to the list
-                images.append(image)
-
-        # Stich the images together
-        # Create a new image
-        new_image = Image.new("RGB", (2048, 1024))
-        # Paste the images
-        new_image.paste(images[0], (0, 0))
-        new_image.paste(images[1], (0, 512))
-        new_image.paste(images[2], (512, 0))
-        new_image.paste(images[3], (512, 512))
-        new_image.paste(images[4], (1024, 0))
-        new_image.paste(images[5], (1024, 512))
-        new_image.paste(images[6], (1536, 0))
-        new_image.paste(images[7], (1536, 512))
+            # Save the image as the name Address
+            new_image.save(f"images/{country}/{address}.png")
+            im += 1
+            # Reload the page
+            driver.refresh()
+            print(f"Saved {address}") 
 
 
-        # Save the image as the name Address
-        new_image.save(f"images/{country}/{address}.png")
+if __name__ == "__main__":
+    from multiprocessing import Process
+    p1 = Process(target=image_getter, args=("dk",))
+    p1.start()
+    p2 = Process(target=image_getter, args=("dk",))
+    p2.start()
+    p3 = Process(target=image_getter, args=("no",))
+    p3.start()
+    p4 = Process(target=image_getter, args=("no",))
+    p4.start()
+    p5 = Process(target=image_getter, args=("se",))
+    p5.start()
+    p6 = Process(target=image_getter, args=("se",))
+    p6.start()
+    p7 = Process(target=image_getter, args=("de",))
+    p7.start()
+    p8 = Process(target=image_getter, args=("de",))
+    p8.start()
 
-        # Reload the page
-        driver.refresh()
+    p1.join()
+    p2.join()
+    p3.join()
+    p4.join()
+    p5.join()
+    p6.join()
+    p7.join()
+    p8.join()
